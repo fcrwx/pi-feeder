f34ccc6c00cc66c0661d63fdd3de53e208ac063a
Karl Olson <karl.olson@ca.com>
2016-10-10 14:20:09 -0600

    remove commented lines

--- power-on.py
+++ power-on.py
@@ -1,10 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 print "power on"
 GPIO.output(18,GPIO.HIGH)
-#print "power off"
-#GPIO.output(18,GPIO.LOW)
